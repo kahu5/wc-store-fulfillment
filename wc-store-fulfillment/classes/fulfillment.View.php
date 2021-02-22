14de3712b1c9f9928fdbe3fb09a7cd3ad57c9bec
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-21 20:25:05 -0500

    Update fulfillment.View.php

--- wc-store-fulfillment/classes/fulfillment.View.php
+++ wc-store-fulfillment/classes/fulfillment.View.php
@@ -1,9 +1,21 @@
 <?php
-function wporg_options_page_html() {
-    ?>
-    <div class="wrap">
-      <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-<p>Just testing for now</p>
-    </div>
-    <?php
+
+defined('ABSPATH') || exit;
+
+require_once __DIR__ . '/../model/LMS.model.php';
+
+class Products
+{
+
+	function wporg_options_page_html() {
+		?>
+		<div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+	<p>Just testing for now...</p>
+	<h3>Current Orders</h3>
+	<br>
+		</div>
+		<?php
+	}
+
 }

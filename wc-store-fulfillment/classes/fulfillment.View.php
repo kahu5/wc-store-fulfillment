845a3d6c9f3d00e0b8c39d7bcce76b6113e5bfc0
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-22 08:21:48 -0500

    Update fulfillment.View.php

--- wc-store-fulfillment/classes/fulfillment.View.php
+++ wc-store-fulfillment/classes/fulfillment.View.php
@@ -1,13 +1,14 @@
 <?php
 
+namespace fulfillmentview;
+
 defined('ABSPATH') || exit;
 
 require_once __DIR__ . '/../classes/fulfillment.Model.php';
 
 class Products
 {
-
-	function wporg_options_page_html() {
+	function options_page_html() {
 		?>
 		<div class="wrap">
 		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>

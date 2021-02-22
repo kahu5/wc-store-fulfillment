3acf54d1389bfec7ef90246a9663dfcd4772a3f9
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-21 20:15:11 -0500

    Update fulfillment.View.php

--- wc-store-fulfillment/classes/fulfillment.View.php
+++ wc-store-fulfillment/classes/fulfillment.View.php
@@ -3,7 +3,7 @@ function wporg_options_page_html() {
     ?>
     <div class="wrap">
       <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-<p></p>
+<p>Just testing for now</p>
     </div>
     <?php
 }

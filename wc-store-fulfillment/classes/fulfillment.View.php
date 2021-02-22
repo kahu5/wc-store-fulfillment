dc256c02878db2e79081bc47e0d7d9968ef7dafa
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-22 09:05:18 -0500

    Update fulfillment.View.php

--- wc-store-fulfillment/classes/fulfillment.View.php
+++ wc-store-fulfillment/classes/fulfillment.View.php
@@ -12,11 +12,24 @@ class Products
 		?>
 		<div class="wrap">
 		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-	<p>Just testing for now...</p>
-	<h3>Current Orders</h3>
-	<br>
+		<p>Just testing for now...</p>
+		<h3>Current Orders</h3>
+		<br>
 		</div>
 		<?php
+	}
+
+	function page_refresh() {
+		$output ='<div class="wrap" style="float:right">
+		<button><span id="refreshIcon" onclick="refreshPage()" class="dashicons dashicons-image-rotate"></span></button>
+		</div>';
+		echo $output;
+	}
+
+	function product_list() {
+		$output ='<table><tr><th>Product</th><th>Number of Orders</th></tr>
+		<tr><td>test1</td><td>test2</td></tr></table>';
+		echo $output;
 	}
 
 }

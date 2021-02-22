5bb106d9071c3cbb2bf5042018d6652d75066370
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-21 20:25:29 -0500

    Update fulfillment.Model.php

--- wc-store-fulfillment/classes/fulfillment.Model.php
+++ wc-store-fulfillment/classes/fulfillment.Model.php
@@ -1,1 +1,24 @@
 <?php
+
+defined('ABSPATH') || exit;
+
+class Products
+{
+// Gets all products that are published.
+
+	public static function getOrders()
+    {
+        global $wpdb;
+        $query = "SELECT p.ID AS order_id, post_date
+            FROM
+            {$wpdb->prefix}posts AS p
+            INNER JOIN {$wpdb->prefix}woocommerce_order_items AS woi ON p.ID = woi.order_id
+            WHERE
+                p.post_type = 'shop_order' AND
+                p.post_status IN ('" . implode("','", array_keys(wc_get_order_statuses())) . "') AND
+                woi.order_item_type = 'coupon' AND
+                woi.order_item_name = ''";
+        $orders = $wpdb->get_results($query);
+        return $orders;
+    }
+}

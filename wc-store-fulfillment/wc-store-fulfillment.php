05ea044be1f273e2eb655422bcdec105f430ace2
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-22 08:22:14 -0500

    Update wc-store-fulfillment.php

--- wc-store-fulfillment/wc-store-fulfillment.php
+++ wc-store-fulfillment/wc-store-fulfillment.php
@@ -17,15 +17,19 @@ require_once __DIR__ . "/classes/fulfillment.View.php";
 
 plugins_url( 'scripts.js', _FILE_ );
 
-add_action( 'admin_menu', 'wporg_options_page' );
-function wporg_options_page() {
+add_action('admin_menu', 'options_page');
+function options_page() {
     add_menu_page(
         'WC Store Fulfillment',
         'Fulfillment',
         'manage_options',
         'wcstorefulfillment',
-        'wporg_options_page_html',
+        'options_page_display',
         'dashicons-clipboard',
-        26
+        24
     );
+}
+
+function options_page_display() {
+    \fulfillmentview\Products::options_page_html();
 }

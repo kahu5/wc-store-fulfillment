3791ade0d0e4fad5ee869c6e13d4fd82175eef6c
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-21 20:14:46 -0500

    Update wc-store-fulfillment.php

--- wc-store-fulfillment/wc-store-fulfillment.php
+++ wc-store-fulfillment/wc-store-fulfillment.php
@@ -12,6 +12,9 @@ Author URI: https://github.com/kahu5/wc-store-fulfillment
 License: GPLv3
 */
 
+require_once __DIR__ . "/classes/fulfillment.Model.php";
+require_once __DIR__ . "/classes/fulfillment.View.php";
+
 plugins_url( 'scripts.js', _FILE_ );
 
 add_action( 'admin_menu', 'wporg_options_page' );
@@ -23,6 +26,6 @@ function wporg_options_page() {
         'wcstorefulfillment',
         'wporg_options_page_html',
         'dashicons-clipboard',
-        20
+        24
     );
 }

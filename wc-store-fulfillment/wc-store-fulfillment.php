9f264a602fbcb7336cb2de5181005db53fbff07d
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-23 13:57:26 -0500

    Update wc-store-fulfillment.php

--- wc-store-fulfillment/wc-store-fulfillment.php
+++ wc-store-fulfillment/wc-store-fulfillment.php
@@ -6,12 +6,14 @@
  * Plugin Name: WC Store Fulfillment
  * Plugin URI: https://github.com/kahu5/wc-store-fulfillment
  * Description: Wordpress plugin for WooCommerce current inventory orders
- * Version: 0.0.5
- * Requires at least: 5.5
- * Requires PHP:      7.3
  * Author: Jared Meidal
  * Author URI: https://github.com/kahu5/wc-store-fulfillment
  * License: GPLv3
+ * Version: 0.0.5
+ * Requires at least: 5.5
+ * Requires PHP:      7.3
+ * WC requires at least: 4.7.0
+ * WC tested up to: 5.0.0
 */
 
 require_once __DIR__ . "/classes/fulfillment.Model.php";
@@ -38,7 +40,9 @@ function options_page() {
 }
 
 function options_page_display() {
-    \fulfillmentview\Products::options_page_html();
 	\fulfillmentview\Products::page_refresh();
-	\fulfillmentview\Products::product_list();
+    \fulfillmentview\Products::options_page_html();
+    $data = \fulfillmentmodel\Products::getPublishedProducts();
+	\fulfillmentview\Products::product_table_header();
+    \fulfillmentview\Products::displayPublishedProducts($data);
 }

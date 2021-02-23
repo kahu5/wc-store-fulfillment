1b65f3c324c50dcbbc5497fc7bcfe568f38d0c29
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-23 13:57:43 -0500

    Update fulfillment.View.php

--- wc-store-fulfillment/classes/fulfillment.View.php
+++ wc-store-fulfillment/classes/fulfillment.View.php
@@ -12,8 +12,7 @@ class Products
 		?>
 		<div class="wrap">
 		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-		<p>Just testing for now...</p>
-		<h3>Current Orders</h3>
+		<h3>Current Products</h3>
 		<br>
 		</div>
 		<?php
@@ -26,10 +25,45 @@ class Products
 		echo $output;
 	}
 
-	function product_list() {
-		$output ='<table><tr><th>Product</th><th>Number of Orders</th></tr>
-		<tr><td>test1</td><td>test2</td></tr></table>';
+	function product_table_header() {
+		$output ='<table><tr><th>Product</th><th>Type of Product</th><th>Attributes</th><th>Orders</th></tr>';
 		echo $output;
 	}
+
+	function product_table_end() {
+		$output ='</table>';
+		echo $output;
+	}
+	
+
+	    /**
+     * Displays woocommerce products in a list.
+     */
+    public static function displayPublishedProducts($data)
+    {
+        $output = "";
+        foreach ($data as $d) {
+            $prod = $d->prod;
+            $output .= "<tr>";
+                $output .= "<td><strong>" . $d->post_title . "</strong></td>";
+                $output .= "<td>" . ucwords($d->p_type) . "</td>";
+                $output .= "<td></td>";
+            if ($d->variations) {
+                foreach($d->variations as $v) {
+                    if ($v['attributes']['attribute_ground']) {
+                        $output .= "<td>" . $v['attributes']['attribute_ground'] . "</td>";
+                    } else if ($v['attributes']['attribute_roast']) {
+                        $output .= "<td>" . $v['attributes']['attribute_roast'] . "</td>";
+                    } else if ($v['attributes']['attribute_weight']) {
+                        $output .= "<td>" . $v['attributes']['attribute_weight'] . "</td>";
+                    }
+                }
+            }
+			$output .= "<td style='text-align:center'>#</td>";
+			$output .= "</tr>";
+        }
+    
+        echo $output;
+    }
 
 }

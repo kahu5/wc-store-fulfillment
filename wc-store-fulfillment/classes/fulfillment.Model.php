72a7b2f485014b5bda79812e7003e5aa491a623a
kahu5 <47630569+kahu5@users.noreply.github.com>
2021-02-23 13:57:59 -0500

    Update fulfillment.Model.php

--- wc-store-fulfillment/classes/fulfillment.Model.php
+++ wc-store-fulfillment/classes/fulfillment.Model.php
@@ -6,7 +6,7 @@ defined('ABSPATH') || exit;
 
 class Products
 {
-// Gets all products that are published.
+// Gets all orders that are published.
 	public static function getOrders()
     {
         global $wpdb;
@@ -19,4 +19,35 @@ class Products
         $orders = $wpdb->get_results($query);
         return $orders;
     }
+
+    /**
+     * Gets all products that are published.
+     */
+    public static function getPublishedProducts()
+    {
+        $args = array(
+            'post_type' => 'product',
+            'posts_per_page' => -1,
+            'status' => array( 'private', 'publish' ),
+            'orderby' => 'ID',        
+            'order' => 'DESC'
+        );
+        
+        $posts = get_posts($args);
+
+        foreach ($posts as $p) {
+            // Get the product type
+            $terms = get_the_terms($p->ID, 'product_type');
+            $p->p_type = (!empty($terms)) ? sanitize_title(current($terms)->name) : 'simple';
+            $prod = wc_get_product($p->ID);
+            $p->prod = $prod;
+
+            if ($p->p_type == 'variable' || $p->p_type == 'variable-subscription') {
+                $p->variations = $prod->get_available_variations();
+            }
+
+        }
+        return $posts;
+    }
+
 }
